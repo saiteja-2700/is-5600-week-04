051d7d61effc14162046c6d42492c67a21acf150
saiteja-2700 <saiteja.alishala@slu.edu>
2025-04-16 03:01:58 +0000

    Lab week 4

---
+++ api.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const Products = require('./products')
+const autoCatch = require('./lib/auto-catch')
+
+
+/**
+ * Handle the root route
+ * @param {object} req
+ * @param {object} res
+*/
+function handleRoot(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  }
+  
+  /**
+   * List all products
+   * @param {object} req
+   * @param {object} res
+   */
+  async function listProducts(req, res) {
+    const { offset = 0, limit = 25, tag } = req.query;
+    
+    try {
+        // Pass the limit and offset to the Products service
+        res.json(await Products.list({
+          offset: Number(offset),
+          limit: Number(limit),
+          tag,
+        }))
+      } catch (err) {
+        res.status(500).json({ error: err.message })
+      }
+    }
+
+async function getProduct (req, res, next) {
+    const { id } = req.params
+      
+    try {
+        const product = await Products.get(id)
+        if (!product) {
+          // next() is a callback that will pass the request to the next available route in the stack
+          return next()
+        }
+    
+        return res.json(product)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
+async function createProduct (req, res) {
+    console.log('request body:', req.body)
+    res.json(req.body)
+}
+
+async function deleteProduct(req, res) {
+  const { id } = req.params;
+
+  try {
+      const result = await Products.deleteProduct(id);
+      res.status(202).json(result);
+  } catch (err) {
+      res.status(500).json({ error: err.message });
+  }
+}
+
+async function putProduct(req, res) {
+  const { id } = req.params;
+  const newProductData = req.body;
+
+  try {
+      const result = await Products.putProduct(id, newProductData);
+      res.status(200).json(result);
+  } catch (err) {
+      res.status(500).json({ error: err.message });
+  }
+}
+
+  module.exports = autoCatch({
+    handleRoot,
+    listProducts,
+    getProduct,
+    createProduct,
+    deleteProduct,
+    putProduct,
+  });
